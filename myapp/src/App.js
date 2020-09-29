8ee06522969005ea4bbe0971ffcf5279536111c8
Elom KOSSI <elom.kossi@utbm.fr>
2020-09-30 00:43:45 +0200

    Deleting-Data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -21,11 +21,21 @@ class App extends Component {
     });
   }
 
+  deleteNinja = (id) => {
+    // console.log(id);
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja}/>
         <AddNinja addNinja={this.addNinja}/>
       </div>
     );
