341468f498fa3a2174dfd5cddc20700676e7bd7e
Elom KOSSI <elom.kossi@utbm.fr>
2020-10-05 00:34:03 +0200

    Route-Parameters

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
-  return (
-    <div>
-      <div className="container">
-        <h4 className="center">Home</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+class Home extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          posts: res.data.slice(0,10)
+        });
+      })
+  }
+
+  render(){
+    const { posts } = this.state
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+                <Link to={'/' + post.id}>
+                    <span className="card-title red-text">{post.title}</span>
+                </Link>
+                <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+      <div className="center">No posts to show</div>
+    );
+
+    return (
+      <div>
+        <div className="container">
+          <h4 className="center">Home</h4>
+          {postList}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
